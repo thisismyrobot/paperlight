0658affa9775b20176a960c3987bc391325de5e7
Robert <rwallhead@gmail.com>
2020-01-02 12:22:16 +1100

    I am a wizard (it shows the count).

--- paperlight/paperlight.ino
+++ paperlight/paperlight.ino
@@ -7,13 +7,18 @@
  */
 #include <M5StickC.h>
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
 
 #include "settings.h"
+
+const char* host = "papertrailapp.com";
+const int port = 443;
+String path = String("/api/v1/events/search.json") + papertrailquery;
 
 void setup() {
 
     M5.begin();
-    M5.Axp.ScreenBreath(10);
+    M5.Axp.ScreenBreath(8);
 
     M5.Lcd.print("Connecting");
     WiFi.begin(ssid, password);
@@ -22,7 +27,40 @@ void setup() {
         delay(500);
         M5.Lcd.print(".");
     }
-    M5.Lcd.print("\nConnected!\n");
+    M5.Lcd.print("\n\nConnected!\n");
+
+    M5.Lcd.print("\nLoading");
+    WiFiClientSecure client;
+    client.connect(host, port);
+    client.println(String("GET ") + path + " HTTP/1.1");
+    client.println(String("Host: ") + host);
+    client.println(String("X-Papertrail-Token: ") + papertrailtoken);
+    client.println();
+
+    int count = 0;
+    String body;
+    while (client.connected()) {
+      body = client.readStringUntil('\n');
+
+      int start = 0;
+      while(1) {
+
+        int index = body.indexOf("\"id\"", start);
+        if (index == -1) {
+          break;
+        }
+        
+        count++;
+        start = index + 1;
+      }
+      M5.Lcd.print(".");
+    }
+
+    M5.Lcd.print("\n\nLoaded!\n");
+
+    M5.Lcd.print(String("\nFound: ") + count);
+
+    client.stop();
 }
 
 // the loop routine runs over and over again forever
